4e52bd883b1097a73eeb824de0d01c3ca8fc6aab
gdj_rtr4 <gitanjalijedhe1@gmail.com>
2023-12-03 00:08:15 +0530

    Singleton implementation

--- DesignPatterns/01-Singleton/Singleton.cpp
+++ DesignPatterns/01-Singleton/Singleton.cpp
@@ -1,7 +1,61 @@
+// Example: Logger
+
 #include <iostream>
 
 using namespace std;
 
+class Singleton {
+private:
+	int m_value;
+
+	static Singleton* s_instance;
+
+	Singleton(int value) : m_value(value) {
+
+	}
+
+public:
+	Singleton(const Singleton& other) = delete;
+
+	void operator=(const Singleton &other) = delete;
+
+	static Singleton* get_instance(int value);
+
+	void SomeBusinessLogic() {
+		// code
+	}
+
+	int get_value() const {
+		return m_value;
+	}
+
+};
+
+Singleton* Singleton::s_instance = nullptr;
+
+Singleton* Singleton::get_instance(int value) {
+	if (s_instance == nullptr) {
+		s_instance = new Singleton(value);
+	}
+
+	return s_instance;
+}
+
+void test_1() {
+	Singleton *ptrSingleton = Singleton::get_instance(848);
+	std::cout << "Inside test-01: " << ptrSingleton->get_value() << std::endl;
+}
+
+void test_2() {
+	Singleton* ptrSingleton = Singleton::get_instance(393);
+	std::cout << "Inside test-02: " << ptrSingleton->get_value() << std::endl;
+}
+
 int main() {
+
+	test_1();
+
+	test_2();
+
 	return 0;
 }
